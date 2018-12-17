3d10da3d94a23a09ebe7e35edaaa39a16fbf30b4
Daniel Miró Pettican <dmirop@uoc.edu>
2018-12-17 23:46:10 +0100

    Code clean up and documentation

--- venv/src/ga.py
+++ venv/src/ga.py
@@ -10,11 +10,18 @@ import time
 
 
 def create_file_name():
+    """
+    Creates a string to identify de file name
+    :return: a string that contains the full date
+    """
     readable = (str(time.ctime(time.time()))).replace(":", "_")
     return readable
 
 
 class GeneticAlgorithm:
+    """
+    Defines a generic Genetic Algorithm
+    """
     def __init__(self, pool_size=100, p_cross=0.8, p_muta=0.05, elitism=True, endogamy=True, max_gen=5000, max_change=1500):
         self._pool = pool.Pool()
         self._pool_size = pool_size
@@ -41,59 +48,14 @@ class GeneticAlgorithm:
         raise NotImplementedError
 
     def run(self):
-        start = time.time()
-        base_file_name = create_file_name()
-
-        parameters = open(base_file_name + "_param", "w")
-        parameters.write(self.get_parameters())
-        parameters.close()
-
-        chromosomes = open(base_file_name + "_chrom", "w")
-
-        log = open(base_file_name + "_log", "w")
-        log.write("MIN, MAX, SUM, MEAN, MEDIAN\n")
-
-        self.initialize()
-
-        generation_number = 0
-        generations_no_changes = 0
-
-        best_chromosome = cp.copy(self._pool.get_best_chromosome())
-        chromosomes.write(self.generate_chromosome_info(best_chromosome))
-        min_evaluation = best_chromosome.get_evaluation()
-
-        log.write(str(self._pool.get_stats()).strip("()") + "\n")
-
-        while (generation_number < self._max_gen) and (generations_no_changes < self._max_change):
-            self.select_and_reproduce()
-            if min_evaluation <= min(self._pool.get_evaluations()):
-                generations_no_changes += 1
-            else:
-                min_evaluation = min(self._pool.get_evaluations())
-                best_chromosome = cp.copy(self._pool.get_best_chromosome())
-                chromosomes.write(self.generate_chromosome_info(best_chromosome))
-                print(
-                    "Better chromosome found at generation {0} with score {1}".format(
-                        generation_number, best_chromosome.get_evaluation()))
-                generations_no_changes = 0
-            generation_number += 1
-
-            log.write(str(self._pool.get_stats()).strip("()") + "\n")
-
-            if generation_number % 100 == 0:
-                stats = self._pool.get_stats()
-                print("Generation {0}: {1} min score, {2} max score, {3} sum score, {4} mean score, {5} median score"
-                      .format(generation_number, stats[0], stats[1], stats[2], stats[3], stats[4]))
-        end = time.time()
-        print("The best chromosome is {0} with score {1} after {2} generations in {3} seconds"
-              .format(best_chromosome.get_gene_sequence(), min_evaluation, generation_number, round(end-start)))
-
-        chromosomes.close()
-        log.close()
-        return best_chromosome
+        raise NotImplementedError
 
 
 class AssignmentsGA(GeneticAlgorithm):
+    """
+    Defines a Genetic Algorithm to assign nurses to patients
+    """
+
     def __init__(self, rooms, distance_matrix, patients, pool_size=100, p_cross=0.8, p_muta=0.05, elitism=True,
                  endogamy=True, max_gen=5000, max_change=1500, nurses=4, w_loads=1, w_dist=1):
         super().__init__(pool_size, p_cross, p_muta, elitism, endogamy, max_gen, max_change)
@@ -103,8 +65,15 @@ class AssignmentsGA(GeneticAlgorithm):
         self._nurses = nurses
         self._w_loads = w_loads
         self._w_dist = w_dist
+        self._reproduce_p = [self._p_cross, 1 - self._p_cross]
+        self._mutate_p = [self._p_muta, 1 - self._p_muta]
+        self._verbose = False;
 
     def get_parameters(self):
+        """
+        Retrieves the values of different parameters from the genetic algorithm
+        :return: a string with all the required information
+        """
         pool_size = str("Pool size: {0}\n".format(self._pool_size))
         p_cross = str("Crossover probability: {0}\n".format(self._p_cross))
         p_muta = str("Mutation probability: {0}\n".format(self._p_muta))
@@ -117,6 +86,10 @@ class AssignmentsGA(GeneticAlgorithm):
         return pool_size + p_cross + p_muta + elitism + endogamy + max_gen + max_change + w_loads + w_dist
 
     def initialize(self):
+        """
+        Creates the first pool to evaluate
+        :return: a random pool of chromosmes
+        """
         base_population = [i for i in range(len(self._patients))]
 
         for nurse in range(-1, -self._nurses, -1):
@@ -129,9 +102,17 @@ class AssignmentsGA(GeneticAlgorithm):
             starting_chromosome = chroms.AssignmentChromosome(gene_sequence)
             starting_chromosome.set_evaluation(self.evaluate(starting_chromosome))
             starting_pool.add_chromosome(starting_chromosome)
+
+        starting_pool.calculate_enter_p()
+
         self._pool = starting_pool
 
     def generate_chromosome_info(self, chromosome):
+        """
+        Generates the information about a chromosome so it's readable
+        :param chromosome: the crhomosome that needs its information polled
+        :return: a string that contains the fitting score as well as the assignments
+        """
         header = "Chromosome with score {0}\n".format(chromosome.get_evaluation())
         assigns = chromosome.get_assignments()
         loads = self.transform_assignments_loads(assigns)
@@ -154,6 +135,11 @@ class AssignmentsGA(GeneticAlgorithm):
         return header + info
 
     def evaluate(self, chromosome):
+        """
+        Evaluates a chromosome taking into account the fitting function
+        :param chromosome: chromosome to be evaluated
+        :return: the value of the fitting function for that chromosome
+        """
         nurse_assignments = chromosome.get_assignments()
         loads = self.calculate_load(nurse_assignments)
         distance = sum(self.calculate_distances(nurse_assignments))
@@ -162,31 +148,40 @@ class AssignmentsGA(GeneticAlgorithm):
         return evaluation
 
     def calculate_load(self, assignments):
+        """
+        Calculates the load of care from the different assignments of patients
+        :param assignments: sets of assignments
+        :return: the standard variance of the cost of cares
+        """
 
         loads = self.transform_assignments_loads(assignments)
-
         loads = [sum(load) for load in loads]
-
         mean = sum(loads) / len(loads)
-
         sq_differences = [(x - mean) ** 2 for x in loads]
-
         variance = sum(sq_differences) / len(sq_differences)
 
         return sqrt(variance)
 
     def transform_assignments_loads(self, assignments):
-
         return [[int(self._patients[index]) for index in assign] for assign in assignments]
 
     def calculate_distances(self, assignments):
+        """
+        Calculates the total distance that all nurses will need to connect all patients
+        :param assignments: sets of patient assignments to nurses
+        :return: the sum of the vaule of the minimal trees
+        """
         nurse_distances = []
         for assign in assignments:
             nurse_distances.append(self.get_pst(assign))
         return nurse_distances
 
     def get_pst(self, assign):
-
+        """
+        Calculates the cost of the primordial tree
+        :param assign: an assignment of patients for a nurse
+        :return: the minimal cost of the tree that joins all patients
+        """
         if len(assign) != 0:
             included_nodes = {}
             priority_queue = []
@@ -211,70 +206,110 @@ class AssignmentsGA(GeneticAlgorithm):
             return 0
 
     def select_and_reproduce(self):
-
-        best_chromosome = cp.copy(self._pool.get_best_chromosome())
-
-        evaluations = self._pool.get_evaluations()
-        sum_evaluations = sum(evaluations)
-
-        if sum_evaluations > 0:
-            converting_factor = max(evaluations) + min(evaluations)
-            enter_p = [round((converting_factor - evaluation) / sum_evaluations, 4) for evaluation in evaluations]
-        else:
-            enter_p = evaluations
-
-        reproduce_p = [self._p_cross, 1 - self._p_cross]
-
-        mutate_p = [self._p_muta, 1 - self._p_muta]
+        """
+        Creates a new pool of children chromosome from the actual pool
+        :return: A new pool with child chromosomes
+        """
 
         if self._elitism:
-            candidates_list = choices(self._pool.get_pool_list(), weights=enter_p, k=self._pool_size - 1)
+            candidates_list = choices(self._pool.get_pool_list(), weights=self._pool.get_enter_p(), k=self._pool_size - 1)
         else:
-            candidates_list = choices(self._pool.get_pool_list(), weights=enter_p, k=self._pool_size)
+            candidates_list = choices(self._pool.get_pool_list(), weights=self._pool.get_enter_p(), k=self._pool_size)
 
         new_pool = pool.Pool()
 
         for candidate in candidates_list:
-            candidate = cp.copy(candidate)
-            reproduce = choices(["cross", "no_change"], weights=reproduce_p)
+            parent = cp.copy(candidate)
+            reproduce = choices(["cross", "no_change"], weights=self._reproduce_p)
             if reproduce[0] == "cross":
                 if not self._endogamy:
-                    parent = cp.copy(choices(self._pool.get_pool_list(), weights=enter_p, k=1)[0])
+                    partner = cp.copy(choices(self._pool.get_pool_list(), weights=self._pool.get_enter_p(), k=1)[0])
+                else:
+                    partner = self._pool.get_best_chromosome()
                 crossover_type = choice(["ordered", "simple", "double"])
                 if crossover_type == "ordered":
-                    if self._endogamy:
-                        candidate.ordered_crossover(best_chromosome)
-                    else:
-                        candidate.ordered_crossover(parent)
+                    parent.ordered_crossover(partner)
                 elif crossover_type == "simple":
-                    if self._endogamy:
-                        candidate.simple_crossover(best_chromosome)
-                    else:
-                        candidate.simple_crossover(parent)
+                    parent.simple_crossover(partner)
                 elif crossover_type == "double":
-                    if self._endogamy:
-                        candidate.double_crossover(best_chromosome)
-                    else:
-                        candidate.double_crossover(parent)
+                    parent.double_crossover(partner)
             elif reproduce[0] == "no_change":
                 pass
 
-            mutate = choices(["muta", "no_change"], weights=mutate_p)
+            mutate = choices(["muta", "no_change"], weights=self._mutate_p)
             if mutate[0] == "muta":
-                candidate.mutate()
+                parent.mutate()
             elif mutate[0] == "no_change":
                 pass
 
-            candidate.set_evaluation(self.evaluate(candidate))
-            new_pool.add_chromosome(candidate)
+            parent.set_evaluation(self.evaluate(parent))
+            new_pool.add_chromosome(parent)
 
         if self._elitism:
-            new_pool.add_chromosome(best_chromosome)
+            new_pool.add_chromosome(cp.copy(self._pool.get_best_chromosome()))
+
+        new_pool.calculate_enter_p()
 
         self._pool = new_pool
 
+    def run(self):
+        start = time.time()
+        base_file_name = create_file_name()
+
+        parameters = open(base_file_name + "_param_asig", "w")
+        parameters.write(self.get_parameters())
+        parameters.close()
+
+        chromosomes = open(base_file_name + "_chrom_asig", "w")
+
+        log = open(base_file_name + "_log_asig", "w")
+        log.write("MIN, MAX, SUM, MEAN, MEDIAN\n")
+
+        self.initialize()
+
+        generation_number = 0
+        generations_no_changes = 0
+
+        best_chromosome = cp.copy(self._pool.get_best_chromosome())
+        chromosomes.write(self.generate_chromosome_info(best_chromosome))
+        min_evaluation = best_chromosome.get_evaluation()
+
+        log.write(str(self._pool.get_stats()).strip("()") + "\n")
+
+        while (generation_number < self._max_gen) and (generations_no_changes < self._max_change):
+            self.select_and_reproduce()
+            if min_evaluation <= min(self._pool.get_evaluations()):
+                generations_no_changes += 1
+            else:
+                min_evaluation = min(self._pool.get_evaluations())
+                best_chromosome = cp.copy(self._pool.get_best_chromosome())
+                chromosomes.write(self.generate_chromosome_info(best_chromosome))
+                if self._verbose:
+                    print("Better chromosome found at generation {0} with score {1}".format(
+                        generation_number, best_chromosome.get_evaluation()))
+                generations_no_changes = 0
+            generation_number += 1
+
+            log.write(str(self._pool.get_stats()).strip("()") + "\n")
+
+            if generation_number % 100 == 0 and self._verbose:
+                stats = self._pool.get_stats()
+                print("Generation {0}: {1} min score, {2} max score, {3} sum score, {4} mean score, {5} median score"
+                      .format(generation_number, stats[0], stats[1], stats[2], stats[3], stats[4]))
+        end = time.time()
+        if self._verbose:
+                print("The best chromosome is {0} with score {1} after {2} generations in {3} seconds"
+                      .format(best_chromosome.get_gene_sequence(), min_evaluation, generation_number, round(end-start)))
+
+        chromosomes.close()
+        log.close()
+        return best_chromosome
+
 
 class UbicationGA(GeneticAlgorithm):
+    """
+    Defines a Genetic Algorithm to explore better ubications for patients
+    """
     def __init__(self, rooms, distance_matrix, patients, pool_size=100, p_cross=0.8, p_muta=0.05, elitism=True,
                  endogamy=True, max_gen=5000, max_change=1500, nurses=4, w_loads=1, w_dist=1):
         super().__init__(pool_size, p_cross, p_muta, elitism, endogamy, max_gen, max_change)
@@ -284,6 +319,7 @@ class UbicationGA(GeneticAlgorithm):
         self._nurses = nurses
         self._w_loads = w_loads
         self._w_dist = w_dist
+        self._verbose = True
 
     def get_parameters(self):
         pool_size = str("Pool size: {0}\n".format(self._pool_size))
@@ -298,11 +334,14 @@ class UbicationGA(GeneticAlgorithm):
         return pool_size + p_cross + p_muta + elitism + endogamy + max_gen + max_change + w_loads + w_dist
 
     def initialize(self):
+        """
+        Creates the first pool of chromosomes
+        :return: a pool with chromosomes with a swapping of patients
+        """
         starting_pool = pool.Pool()
 
-        starting_gene_sequence = [i for i in range(len(self._patients))]
-
         for population in range(self._pool_size-1):
+            starting_gene_sequence = [i for i in range(len(self._patients))]
             starting_chromosome = chroms.Chromosome(starting_gene_sequence)
             starting_chromosome.mutate()
             starting_chromosome.set_evaluation(self.evaluate(starting_chromosome))
@@ -312,13 +351,21 @@ class UbicationGA(GeneticAlgorithm):
         starting_chromosome.set_evaluation(self.evaluate(starting_chromosome))
         starting_pool.add_chromosome(starting_chromosome)
 
+        starting_pool.calculate_enter_p()
+
         self._pool = starting_pool
 
     def generate_chromosome_info(self, chromosome):
         header = "Chromosome with score {0}\n".format(chromosome.get_evaluation())
-        return header
+        info = "{0} \n \n".format(chromosome.get_gene_sequence())
+        return header + info
 
     def evaluate(self, chromosome):
+        """
+        Calculates the fitting value for a chromosome
+        :param chromosome: the patient distribution from where to calculate its value
+        :return: the value of the chromosome
+        """
         new_patient_list = self.map_loads(chromosome.get_gene_sequence())
         assignment_chromosome = AssignmentsGA(self._rooms, self._distance_matrix, new_patient_list,
                                               nurses=self._nurses).run()
@@ -329,24 +376,75 @@ class UbicationGA(GeneticAlgorithm):
         return loads
 
     def select_and_reproduce(self):
-        best_chromosome = cp.copy(self._pool.get_best_chromosome())
+        """
+        Creates a new pool of child chromosomes
+        :return: a new pool from where to keep calculating
+        """
 
-        evaluations = self._pool.get_evaluations()
-        sum_evaluations = sum(evaluations)
-
-        if sum_evaluations > 0:
-            converting_factor = max(evaluations) + min(evaluations)
-            enter_p = [round((converting_factor - evaluation) / sum_evaluations, 4) for evaluation in evaluations]
-        else:
-            enter_p = evaluations
-
-        candidates_list = choices(self._pool.get_pool_list(), weights=enter_p, k=self._pool_size)
+        candidates_list = choices(self._pool.get_pool_list(), weights=self._pool.get_enter_p(), k=self._pool_size)
 
         new_pool = pool.Pool()
 
         for candidate in candidates_list:
+            print(candidate.get_gene_sequence())
             candidate.mutate()
+            print(candidate.get_gene_sequence())
             candidate.set_evaluation(self.evaluate(candidate))
             new_pool.add_chromosome(candidate)
 
+        new_pool.calculate_enter_p()
+
         self._pool = new_pool
+
+    def run(self):
+        start = time.time()
+        base_file_name = create_file_name()
+
+        parameters = open(base_file_name + "_param_ubi", "w")
+        parameters.write(self.get_parameters())
+        parameters.close()
+
+        chromosomes = open(base_file_name + "_chrom_ubi", "w")
+
+        log = open(base_file_name + "_log_ubi", "w")
+        log.write("MIN, MAX, SUM, MEAN, MEDIAN\n")
+
+        self.initialize()
+
+        generation_number = 0
+        generations_no_changes = 0
+
+        best_chromosome = cp.copy(self._pool.get_best_chromosome())
+        chromosomes.write(self.generate_chromosome_info(best_chromosome))
+        min_evaluation = best_chromosome.get_evaluation()
+
+        log.write(str(self._pool.get_stats()).strip("()") + "\n")
+
+        while (generation_number < self._max_gen) and (generations_no_changes < self._max_change):
+            self.select_and_reproduce()
+            if min_evaluation <= min(self._pool.get_evaluations()):
+                generations_no_changes += 1
+            else:
+                min_evaluation = min(self._pool.get_evaluations())
+                best_chromosome = cp.copy(self._pool.get_best_chromosome())
+                chromosomes.write(self.generate_chromosome_info(best_chromosome))
+                if self._verbose:
+                    print("Better chromosome found at generation {0} with score {1}".format(
+                        generation_number, best_chromosome.get_evaluation()))
+                generations_no_changes = 0
+            generation_number += 1
+
+            log.write(str(self._pool.get_stats()).strip("()") + "\n")
+
+            if generation_number % 100 == 0 and self._verbose:
+                stats = self._pool.get_stats()
+                print("Generation {0}: {1} min score, {2} max score, {3} sum score, {4} mean score, {5} median score"
+                      .format(generation_number, stats[0], stats[1], stats[2], stats[3], stats[4]))
+        end = time.time()
+        if self._verbose:
+            print("The best chromosome is {0} with score {1} after {2} generations in {3} seconds"
+                  .format(best_chromosome.get_gene_sequence(), min_evaluation, generation_number, round(end-start)))
+
+        chromosomes.close()
+        log.close()
+        return best_chromosome
--- venv/src/pool.py
+++ venv/src/pool.py
@@ -5,8 +5,13 @@ class Pool:
     def __init__(self):
         self._pool_list = []
         self._best_chromosome = None
+        self._enter_p = None
 
     def add_chromosome(self, chromosome):
+        """
+        Method to add a new chromosome to the pool
+        :param chromosome: chromosome to add
+        """
         self._pool_list.append(chromosome)
         if self._best_chromosome is None:
             self._best_chromosome = chromosome
@@ -26,6 +31,10 @@ class Pool:
         return max([chromosome.get_evaluation() for chromosome in self._pool_list])
 
     def get_stats(self):
+        """
+        Method to retrieve the statistics of the pool
+        :return: a tuple of statistics
+        """
         evaluations = self.get_evaluations()
         return min(evaluations), max(evaluations), sum(evaluations), sum(evaluations)/len(evaluations), \
             sorted(evaluations)[round(len(evaluations)/2)]
@@ -35,3 +44,20 @@ class Pool:
 
     def set_pool(self, pool_list):
         self._pool_list = pool_list
+
+    def calculate_enter_p(self):
+        """
+        Calculates the probability to be chosen for the next generation pool
+        :return: a list with the probabilities
+        """
+        evaluations = self.get_evaluations()
+        sum_evaluations = sum(evaluations)
+
+        if sum_evaluations > 0:
+            converting_factor = max(evaluations) + min(evaluations)
+            self._enter_p = [round((converting_factor - evaluation) / sum_evaluations, 4) for evaluation in evaluations]
+        else:
+            self._enter_p = evaluations
+
+    def get_enter_p(self):
+        return self._enter_p
